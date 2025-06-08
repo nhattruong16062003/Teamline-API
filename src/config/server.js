caf6b7d2cbd8d2ea515b309850ee07f3cc3c7bdf
PhamVanDu <phamvandu1m78@gmail.com>
2025-06-08 17:51:07 +0700

    config cros and tempalte engine for server

--- src/config/server.js
+++ src/config/server.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const indexRoute = require("../routes/index.routes");
 const connectDB = require("../config/db");
 const app = express();
 
-require("dotenv").config();
-connectDB();
 app.use(express.json());
+require("dotenv").config();
 app.use(morgan("dev"));
+
+//Cấu hình cros để be có thể nhận request từ fe
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Các phương thức HTTP được phép
+    allowedHeaders: ["Content-Type", "Authorization"], // Các header được phép
+    credentials: true, // Cho phép gửi cookie
+  })
+);
+
+//Thiết lập Pug làm template engine
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+connectDB();
 app.use("/api", indexRoute);
 
 module.exports = app;
-
