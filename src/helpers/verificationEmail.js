71f610e741157981fde08771105b3bc94334be49
PhamVanDu <phamvandu1m78@gmail.com>
2025-06-11 07:18:53 +0700

    auth done

--- src/helpers/verificationEmail.js
+++ src/helpers/verificationEmail.js
@@ -6,13 +6,13 @@ const sendEmail = require("../services/emailSender");
 const prepareSendVerificationEmail = async (req, res, user) => {
   try {
     const verifyToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1d",
+      expiresIn: "15m",
     });
     const html = pug.renderFile(
       path.join(__dirname, "../views", "verify-email.pug"),
       {
         name: user.username,
-        verifyUrl: `${process.env.URL_BE}/api/auth/verify-email?token=${verifyToken}`,
+        verifyUrl: `${process.env.URL_FE}/verify-email?token=${verifyToken}`,
       }
     );
     //Gửi email xác thực tài khoản
