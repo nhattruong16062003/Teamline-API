185d7ef1981417c43637143b579f269061634085
PhamVanDu <phamvandu1m78@gmail.com>
2025-06-13 15:07:02 +0700

    search user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   updateUserInfo,
   changePassword,
+  getUserByEmail,
 } = require("../controllers/user.controllers");
 
+router.get("/:email", getUserByEmail);
 router.put("/:id", updateUserInfo);
 router.patch("/change-password/:id", changePassword);
 
