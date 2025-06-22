9ac6699884298a5a9951f84be4cbf04c6ff7f118
PhamVanDu <phamvandu1m78@gmail.com>
2025-06-23 00:59:17 +0700

    dismiss notification

--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -4,10 +4,14 @@ const {
   getNotifications,
   markMultipleNotificationsAsRead,
   markAllNotificationsAsRead,
+  dismissNotification,
+  dismissMultipleNotifications,
 } = require("../controllers/notification.controllers");
 
 router.get("/", getNotifications);
-router.post("/mark-read", markMultipleNotificationsAsRead);
-router.post("/mark-all-read", markAllNotificationsAsRead);
+router.patch("/mark-read", markMultipleNotificationsAsRead);
+router.patch("/mark-all-read", markAllNotificationsAsRead);
+router.patch("/dismiss-multiple", dismissMultipleNotifications);
+router.patch("/dismiss/:id", dismissNotification);
 
 module.exports = router;
