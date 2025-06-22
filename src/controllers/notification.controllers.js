0d59e80d9d1e0cc9caf0de6efbc2f579d4ac2d65
PhamVanDu <phamvandu1m78@gmail.com>
2025-06-22 22:56:52 +0700

    setup auto mark noti isRead

--- src/controllers/notification.controllers.js
+++ src/controllers/notification.controllers.js
@@ -27,6 +27,88 @@ const getNotifications = async (req, res) => {
   res.json({ items, nextCursor });
 };
 
+//Hàm cập nhật trạng thái cho một noti
+const markNotificationAsRead = async () => {
+  try {
+    const { notificationId } = req.body;
+    const updatedNotification = await Notification.findByIdAndUpdate(
+      notificationId,
+      {
+        $set: {
+          isRead: true,
+          readAt: new Date(),
+        },
+      },
+      { new: true } // Trả về document sau khi update
+    );
+
+    return updatedNotification;
+  } catch (error) {
+    throw new Error(`Error marking notification as read: ${error.message}`);
+  }
+};
+
+//Hàm cập nhật trạng thái cho nhiều noti
+const markMultipleNotificationsAsRead = async (req, res) => {
+  try {
+    const { notificationIds } = req.body;
+
+    if (!notificationIds || !Array.isArray(notificationIds)) {
+      return res
+        .status(400)
+        .json({ error: "notificationIds must be an array" });
+    }
+
+    const result = await Notification.updateMany(
+      {
+        _id: { $in: notificationIds },
+        isRead: false,
+      },
+      {
+        $set: {
+          isRead: true,
+          readAt: new Date(),
+        },
+      }
+    );
+
+    res.json({
+      success: true,
+      modifiedCount: result.modifiedCount,
+    });
+  } catch (error) {
+    console.error("Error marking notifications as read:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//Hàm cập nhất isRead cho tất cả thong báo của một người
+const markAllNotificationsAsRead = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const result = await Notification.updateMany(
+      {
+        receiver: userId,
+        isRead: true,
+      },
+      {
+        $set: {
+          isRead: false,
+          readAt: new Date(),
+        },
+      }
+    );
+
+    return result.modifiedCount;
+  } catch (error) {
+    throw new Error(
+      `Error marking all notifications as read: ${error.message}`
+    );
+  }
+};
+
 module.exports = {
   getNotifications,
+  markMultipleNotificationsAsRead,
+  markAllNotificationsAsRead,
 };
--- src/routes/notification.routes.js
+++ src/routes/notification.routes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getNotifications } = require("../controllers/notification.controllers");
+const {
+  getNotifications,
+  markMultipleNotificationsAsRead,
+  markAllNotificationsAsRead,
+} = require("../controllers/notification.controllers");
 
 router.get("/", getNotifications);
+router.post("/mark-read", markMultipleNotificationsAsRead);
+router.post("/mark-all-read", markAllNotificationsAsRead);
 
 module.exports = router;
